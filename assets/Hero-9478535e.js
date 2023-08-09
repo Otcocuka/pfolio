bcd2d2fb76bf189cf214702b226d0409610ece2d
Otcocuka <d.ipharziev@gmail.com>
2023-08-09 20:31:29 +0300

    Updates

--- assets/Hero-9478535e.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-5116342e.js";function s({data:e="Hi there! I'm  Damir Farziev"}){return r.jsx("div",{className:"hero",children:r.jsx("h1",{children:e})})}export{s as H};
---
+++ assets/Hero-ecb09104.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-2a2e97fb.js";function s({data:e="Hi there! I'm  Damir Farziev"}){return r.jsx("div",{className:"hero",children:r.jsx("h1",{children:e})})}export{s as H};
