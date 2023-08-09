ecdd387007df3cb91d02b211ebf0c3fb04424e5c
Otcocuka <d.ipharziev@gmail.com>
2023-08-09 20:30:25 +0300

    added some of my previous works. Tweaked home page, helpYourself page a

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,14 +12,14 @@ const Home = () => {
         workID="0x.bet"
         tags={["building", "motion", "direction"]}
         description="Новинка 2022 года! Современное и функциональное крипто-гейминг с минималистичным дизайном и удобным интерфейсом. Бонусы, круглосуточная поддержка, кешбек, а главное — безопасность! Продукт, который соответствует времени и дает игрокам ровно то, что им нужно."
-        img="../public/0x.png"
+        img="./0x.png"
         button_link="https://0x.bet/"
       />
       <Works
         workID="Betibet.com"
         tags={["building", "motion", "direction"]}
         description="Продукт, сочетающий в себе онлайн-гейминг и платформу для ставок на спорт! Строгий и минималистичный интерфейс позволяет сконцентрироваться на сути игры и позволяет игрокам погружаться в свои эмоции от ставок или игр."
-        img="../public/betibet.png"
+        img="./betibet.png"
         button_link="https://betibet.com/"
       />
 
@@ -27,7 +27,7 @@ const Home = () => {
         workID="Play.betibet.com"
         tags={["building", "motion", "direction"]}
         description="Продукт, сочетающий в себе онлайн-гейминг и платформу для ставок на спорт! Строгий и минималистичный интерфейс позволяет сконцентрироваться на сути игры и позволяет игрокам погружаться в свои эмоции от ставок или игр."
-        img="../public/play-betibet.png"
+        img="./play-betibet.png"
         button_link="https://play.betibet.com/"
       />
 
@@ -35,7 +35,7 @@ const Home = () => {
         workID="Bingobonga.com"
         tags={["building", "motion", "direction"]}
         description="Онлайн-гейминг еще никогда не был таким веселым! Множество увлекательных игр, круглосуточная поддержка, честная и веселая игра, классные бонусы — это лишь малая часть того, что есть в BingoBonga!"
-        img="../public/bingo.png"
+        img="./bingo.png"
         button_link="https://bingobonga.com/"
       />
 
@@ -43,21 +43,21 @@ const Home = () => {
         workID="Jacktop.com"
         tags={["building", "motion", "direction"]}
         description="Приветствуем вас в казино JackTop — самой горячей новинке 2023 года! Мы сделали все возможное, чтобы создать стильный и функциональный онлайн-гейминг проект с большим выбором игр, бонусов и удобным интерфейсом, который добавляет комфорта и удовольствия в игру. Наш уникальный персонаж и дизайн-решения позволят вашим игрокам наслаждаться неповторимым игровым опытом. Будучи новым казино на рынке, JackTop уже привлекло внимание игроков и обещает стать одним из лучших в индустрии."
-        img="../public/jacktop.png"
+        img="./jacktop.png"
         button_link="https://jacktop.com/"
       />
       <Works
         workID="Zotabet.com"
         tags={["building", "motion", "direction"]}
         description="Специально для наших посетителей из Австралии! Продукт, сочетающий в себе онлайн-гейминг и платформу для ставок, стильный и удобный интерфейс позволяет игрокам наслаждаться игрой в полную силу! Круглосуточная поддержка, выгодные бонусы и эмоции, эмоции, эмоции!"
-        img="../public/zotabet.png"
+        img="./zotabet.png"
         button_link="https://zotabet.com/"
       />
       <Works
         workID="Polmetra.ru"
         tags={["building", "motion", "direction"]}
         description="Интернет-магазин с большим ассортиментом межкомнатных дверей и напольных покрытий, более 100 производителей. Продаем продукцию напрямую с 40+ складов наших партнеров. Работаем с юр лицами, доставляем собственным транспортом и через ТК по РФ. Действуют постоянные акции"
-        img="../public/polmetra.png"
+        img="./polmetra.png"
         button_link="https://Polmetra.ru/"
       />
 
@@ -67,7 +67,7 @@ const Home = () => {
         description="Чётко отлаженные поставки, высокий уровень клиентского сервиса, тщательный подбор ассортимента, конкурентные цены – эти и многие другие особенности подхода к делу позволяют нам занимать лидирующие позиции на рынке продажи дверей и напольных покрытий.
 
         Мы сможем индивидуально подобрать разные варианты решения вашего вопроса."
-        img="../public/dveri-i-dot.png"
+        img="./dveri-i-dot.png"
         button_link="https://dveri-i.ru/"
       />
 
@@ -75,7 +75,7 @@ const Home = () => {
         workID="Dovita"
         tags={["building", "motion", "direction"]}
         description="Dovita Bridal is your one-stop solution to getting the perfect attire for the perfect day. Being an exclusive bridal boutique, we serve the Vaughan and Greater Toronto area, Ontario with our wide variety of inspiring wedding and evening dress collections."
-        img="../public/dovita-i.png"
+        img="./dovita-i.png"
         button_link="https://dovitabridalcanada.com/"
       />
 
@@ -84,7 +84,7 @@ const Home = () => {
         tags={["building", "motion", "direction"]}
         description="Компания, занимающаяся созданием и разработкой сайтов, их оптимизацией и последующим продвижением. Наш офис находится в Москве. Комплексная web разработка, изготовление и последующее развитие интернет проекта, оказываемая одним исполнителем, позволит вам снизить затраты, а нам даст возможность с максимальной эффективностью подготовить ресурс для долгой и успешной жизни в интернете.
         "
-        img="../public/codersafe.png"
+        img="./codersafe.png"
         button_link="https://codersafe.ru/"
       />
 
@@ -92,21 +92,21 @@ const Home = () => {
         workID="Uber777.Moscow.ru"
         tags={["building", "motion", "direction"]}
         description="did it long time ago"
-        img="../public/uber.png"
+        img="./uber.png"
         button_link=""
       />
       <Works
         workID="Sanri.ru"
         tags={["building", "motion", "direction"]}
         description="did it long time ago"
-        img="../public/sanri.png"
+        img="./sanri.png"
         button_link=""
       />
       <Works
         workID="Known.sg"
         tags={["building", "motion", "direction"]}
         description="did it long time ago"
-        img="../public/known.png"
+        img="./known.png"
         button_link=""
       />
 
