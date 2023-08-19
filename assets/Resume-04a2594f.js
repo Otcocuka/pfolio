6745c80656d19b4c9e8a6f061c944d4bb8dd3129
Otcocuka <Otcocuka@users.noreply.github.com>
2023-08-19 15:31:57 +0000

    deploy: 81ed39092b6f2fdbdf64cf4699dafdb8b6c8d989

--- assets/Resume-04a2594f.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-b2f43c03.js";const r=()=>e.jsx("div",{className:"resume",children:e.jsx("iframe",{src:"https://hh.ru/resume/a636b5feff0afba7c70039ed1f4b4462356569"})});export{r as default};
---
+++ assets/Resume-7f95b0f0.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-d2b20da1.js";const r=()=>e.jsx("div",{className:"resume",children:e.jsx("iframe",{src:"https://hh.ru/resume/a636b5feff0afba7c70039ed1f4b4462356569"})});export{r as default};
