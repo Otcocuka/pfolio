3e0db0d8a49bdb48627122b4889a4a8787ac792d
Otcocuka <d.ipharziev@gmail.com>
2023-08-21 04:00:27 +0300

    Updates

---
+++ assets/Resume-f722a043.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-62e87181.js";const r=()=>e.jsx("div",{className:"resume",children:e.jsx("iframe",{src:"https://hh.ru/resume/a636b5feff0afba7c70039ed1f4b4462356569"})});export{r as default};
