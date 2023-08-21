4c3815d421caed58f95581d779a802f93e074b5a
Otcocuka <Otcocuka@users.noreply.github.com>
2023-08-21 17:05:45 +0000

    deploy: b5ef35474813c67b40a7b21da46695db09302584

---
+++ assets/Resume-2c2c21c6.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-4413eb0f.js";const r=()=>e.jsx("div",{className:"resume",children:e.jsx("iframe",{src:"https://hh.ru/resume/a636b5feff0afba7c70039ed1f4b4462356569"})});export{r as default};
