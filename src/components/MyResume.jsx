6884947159bab5c6c7b3455515d21b2e2b96659b
Otcocuka <84176865+Otcocuka@users.noreply.github.com>
2025-02-04 14:13:27 +0400

    Update MyResume.jsx

--- src/components/MyResume.jsx
+++ src/components/MyResume.jsx
@@ -32,31 +32,6 @@ const MyResume = () => {
           </motion.h2>
           <div className="resume_block">
             <div className="resume_block_head">
-              <div className="resume_block__company">
-                Digital — integrator 5 CORNERS
-              </div>
-              <div className="resume_block__position">
-                Junior Project Manager
-              </div>
-              <div className="resume_block__date">Apr 2023 - Present</div>
-            </div>
-            <ul className="resume_block_body">
-              <li className="resume_block__achievements">
-                Project planning and coordination
-              </li>
-              <li className="resume_block__achievements">
-                Effective communication
-              </li>
-              <li className="resume_block__achievements">
-                Monitoring, reporting, and risk management
-              </li>
-              <li className="resume_block__achievements">
-                Team leadership and results analysis
-              </li>
-            </ul>
-          </div>
-          <div className="resume_block">
-            <div className="resume_block_head">
               <div className="resume_block__company">Freelance</div>
               <div className="resume_block__position">Frontend-Developer</div>
               <div className="resume_block__date">Mar 2023 - Present</div>
@@ -73,33 +48,17 @@ const MyResume = () => {
             <div className="resume_block_head">
               <div className="resume_block__company">Boomerang-partners</div>
               <div className="resume_block__position">Content-manager</div>
-              <div className="resume_block__date">Aug 2022 - Feb 2023</div>
+              <div className="resume_block__date">Aug 2022 - Mar 2023</div>
             </div>
             <ul className="resume_block_body">
               <li className="resume_block__achievements">
-                Managed and updated content across 7 online casinos, ensuring
-                freshness and relevance of information.
+                Developed responsive web apps for 7 online casinos using React.js and Vue.js
               </li>
               <li className="resume_block__achievements">
-                Collaborated with designers, generated technical briefs, and
-                oversaw task execution.
+                Optimized content management within ComfortableMexicanSofa CMS, Strapi, and custom-built CMS
               </li>
               <li className="resume_block__achievements">
-                Monitored and coordinated work with SEO specialists, ensuring
-                content optimization on websites.
-              </li>
-              <li className="resume_block__achievements">
-                Conducted pre-launch localization testing, ensuring translation
-                accuracy and quality.
-              </li>
-              <li className="resume_block__achievements">
-                Conducted competitor analysis and market research, identifying
-                opportunities for improvement and growth.
-              </li>
-              <li className="resume_block__achievements">
-                Implemented innovations, including layout, integration (projects
-                using React and various CMS platforms), and coordination with
-                the platform-provider&apos;s development team.
+                Improved PWA performance, reducing load times and increasing user engagement
               </li>
             </ul>
           </div>
@@ -109,28 +68,19 @@ const MyResume = () => {
                 LLC &quot;Woodtechretail&quot;
               </div>
               <div className="resume_block__position">
-                Sales Manager, Frontend-Developer
+                Frontend-Developer
               </div>
-              <div className="resume_block__date">Jan 2022 - Jul 2022</div>
+              <div className="resume_block__date">Jan 2022 - Dec 2023</div>
             </div>
             <ul className="resume_block_body">
               <li className="resume_block__achievements">
-                Successful client support and deal closure
+                Developed responsive websites using Bootstrap, PHP, Vanilla JS, and jQuery, ensuring cross-browser compatibility
               </li>
               <li className="resume_block__achievements">
-                Efficient selling of flooring coverings and doors
+                Participated in the migration from OpenCart 2.xx to 3.xx, optimizing CMS content and improving website performance
               </li>
               <li className="resume_block__achievements">
-                Active promotion of products on trading platforms
-              </li>
-              <li className="resume_block__achievements">
-                Professional layout and design of website pages
-              </li>
-              <li className="resume_block__achievements">
-                Content optimization through the administrative panel
-              </li>
-              <li className="resume_block__achievements">
-                Integration with the inventory management system
+                Integrated the inventory management system
                 &quot;MoySklad&quot;
               </li>
             </ul>
